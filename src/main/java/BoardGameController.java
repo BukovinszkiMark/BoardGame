37642bc0cd5dbf08b9bfdbf1c8275303d1f75fb3
BukovinszkiMark <bmbuki7@gmail.com>
2021-05-09 02:09:12 +0200

    Added matrix representation and fixed start position.

--- src/main/java/BoardGameController.java
+++ src/main/java/BoardGameController.java
@@ -1,4 +1,5 @@
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -9,6 +10,8 @@ public class BoardGameController {
     @FXML
     private GridPane board;
 
+    private BoardMatrix matrix;
+
     @FXML
     private void initialize() {
         for (int i = 0; i < board.getRowCount(); i++) {
@@ -17,15 +20,32 @@ public class BoardGameController {
                 board.add(square, j, i);
             }
         }
+        this.matrix = new BoardMatrix(board.getRowCount(), board.getColumnCount());
+        updateBoardFromMatrix(matrix);
     }
 
     private StackPane createSquare() {
         var square = new StackPane();
         square.getStyleClass().add("square");
         var piece = new Circle(30);
-        piece.setFill(Color.RED);
+        piece.setFill(Color.TRANSPARENT);
         square.getChildren().add(piece);
         return square;
+    }
+
+    private void updateBoardFromMatrix(BoardMatrix matrix) {
+        for (Node child : board.getChildren()) {
+            int r = GridPane.getRowIndex(child);
+            int c = GridPane.getColumnIndex(child);
+            StackPane square = (StackPane) child;
+            Circle circle = (Circle) square.getChildren().get(0);
+            switch (matrix.get(r, c)) {
+                case 'e':{circle.setFill(Color.TRANSPARENT);break;}
+                case 'r':{circle.setFill(Color.RED);break;}
+                case 'b':{circle.setFill(Color.BLUE);break;}
+                case 'w':{square.getStyleClass().set(0, "wall");break;}
+            }
+        }
     }
 
 }
---
+++ src/main/java/BoardMatrix.java
@@ -0,0 +1,36 @@
+public class BoardMatrix {
+    /*
+    matrix[row][column]
+
+    values:
+    'e' = empty
+    'r' = red
+    'b' = blue
+    'w' = wall
+     */
+
+    private char[][] matrix;
+
+    public BoardMatrix(int rows, int columns) {
+        this.matrix = new char[rows][columns];
+        this.toStartPosition(rows, columns);
+    }
+
+    public void toStartPosition(int rows, int columns) {
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                matrix[row][column] = 'e';
+            }
+        }
+        matrix[0][0] = 'r';
+        matrix[5][5] = 'r';
+        matrix[0][5] = 'b';
+        matrix[5][0] = 'b';
+        matrix[3][3] = 'w';
+    }
+
+    public char get(int row, int column) {
+        return matrix[row][column];
+    }
+
+}
